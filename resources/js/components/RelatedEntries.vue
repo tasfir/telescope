9054d1f118f27d481ebbfdb77a8da51292f4f488
Tasfir <tasfirsuman@gmail.com>
2026-09-15 16:08:04 +0600

    Add external-link icon to related-queries header
    The empty header cell above the row-action column looked incomplete.
    It now links to the full Queries screen (opened in a new tab), giving
    users a way out of the request-scoped view to browse all queries.

--- resources/js/components/RelatedEntries.vue
+++ resources/js/components/RelatedEntries.vue
@@ -334,6 +334,17 @@ export default {
                         <th class="table-fit">
                             <copy-icon-button v-if="queries.length" :text="allQueriesText(queries)" title="Copy All" />
                         </th>
+                        <th class="table-fit">
+                            <router-link :to="{ name: 'queries' }" target="_blank" class="control-action" title="Open Queries in new tab">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" style="width: 1.25rem; height: 1.25rem">
+                                    <path
+                                        fill-rule="evenodd"
+                                        d="M15.75 2.25H21a.75.75 0 01.75.75v5.25a.75.75 0 01-1.5 0V4.81L8.03 17.03a.75.75 0 01-1.06-1.06L19.19 3.75h-3.44a.75.75 0 010-1.5Zm-10.5 4.5a1.5 1.5 0 00-1.5 1.5v10.5a1.5 1.5 0 001.5 1.5h10.5a1.5 1.5 0 001.5-1.5V10.5a.75.75 0 011.5 0v8.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V8.25a3 3 0 013-3h8.25a.75.75 0 010 1.5H5.25Z"
+                                        clip-rule="evenodd"
+                                    />
+                                </svg>
+                            </router-link>
+                        </th>
                         <th></th>
                     </tr>
                 </thead>
